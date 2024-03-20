715c0e5dece9e19d30fcbb71d0213734bdf51fd4
DzhSmd <semzhid9@gmail.com>
2024-03-21 02:55:57 +0700

    Тест класса Circle метода Distance

---
+++ Paint_2.0.Tests/CircleTest.cs
@@ -0,0 +1,20 @@
+using Paint_2._0.Utilities;
+using System.Net;
+
+namespace Paint_2._0.Tests
+{
+    [TestClass]
+    public class CircleTest
+    {
+        [TestMethod]
+        public void Circle_DistanceTest()
+        {
+            Point2 a = new Point2(2, 2); 
+            Point2 b = new Point2(3, 2); ;
+            double dx = a.X - b.X;
+            double dy = a.Y - b.Y;
+            var expected = 1;
+            Assert.AreEqual(expected, Math.Sqrt(dx * dx + dy * dy));
+        }
+    }
+}
